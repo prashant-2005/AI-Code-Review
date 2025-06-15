e7643252c3654982b907d29b66028e1521d9c447
Prashant Raj <prashantraj01122@gmail.com>
2025-06-16 00:47:33 +0530

    final

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const [reviews, setReviews] = useState(``);
   },[])
 
   async function reviewCode() {
-    const response = await axios.post('https://ai-code-review-yue9.onrender.com', { code })
+    const response = await axios.post('https://ai-code-review-yue9.onrender.com/ai/get-review', { code })
 
       setReviews(response.data)
   }
