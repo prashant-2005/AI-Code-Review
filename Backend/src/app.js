28b5412a6a1e744b4c4ea421d898a28b3118b984
Prashant Raj <prashantraj01122@gmail.com>
2025-06-16 12:10:14 +0530

    url change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,7 @@ app.use(cors(
     {
       origin: [
         "http://localhost:5173",
+        "https://ai-code-review-green.vercel.app"
       ],
       credentials: true
     }
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const [reviews, setReviews] = useState(``);
   },[])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-review-swe8.onrender.com/ai/get-review', { code })
 
 
       setReviews(response.data)
